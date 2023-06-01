7b782674d2c4dcdb5034cf933e370073f28fe8ee
DanielDiazDev <danieldiazdev00@gmail.com>
2023-06-01 19:13:51 -0300

    first version

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,23 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.server.application.Application
-
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import routes.registerCustomerRoutes
+import routes.registerOrderRoutes
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
+    install(ContentNegotiation){
+        json()
+    }
+    registerCustomerRoutes()
+    registerOrderRoutes()
 
 }
+
+
+
+
+
